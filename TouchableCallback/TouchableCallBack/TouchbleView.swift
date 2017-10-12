4e23e0b91da2e511c301989e6c27a03214a4d8f8
tsukudabuddha <andrewtsukuda@gmail.com>
2017-10-12 14:58:35 -0700

    Added completed touchableCallBack

---
+++ TouchableCallback/TouchableCallBack/TouchbleView.swift
@@ -0,0 +1,39 @@
+//
+//  TouchbleView.swift
+//  TouchableDelegation
+//
+//  Created by Eliel A. Gordon on 10/5/17.
+//  Copyright © 2017 MakeSchool. All rights reserved.
+//
+
+import UIKit
+
+typealias TouchCallBack = () -> Void
+
+class TouchbleView: UIView {
+    var touchCallBack: TouchCallBack?
+
+    let touchGesture = UITapGestureRecognizer()
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        touchGesture.addTarget(self, action: #selector(handleTap(tap:)))
+        
+        self.addGestureRecognizer(touchGesture)
+        
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        touchGesture.addTarget(self, action: #selector(handleTap(tap:)))
+        self.addGestureRecognizer(touchGesture)
+        
+    }
+    
+    // Called when view is tapped
+    @objc func handleTap(tap: UITapGestureRecognizer) {
+        touchCallBack?()
+    }
+}
